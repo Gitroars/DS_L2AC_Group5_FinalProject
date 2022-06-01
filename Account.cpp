d15868230b672dcb40714060dabc2a137fded4f2
Gitroars <501sterror@gmail.com>
2022-06-02 05:31:40 +0700

    Initialize

---
+++ Account.cpp
@@ -0,0 +1,97 @@
+//---------------------------------------------------------------------------
+
+#include <fmx.h>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#pragma hdrstop
+
+#include "Account.h"
+#include "Quest.h"
+using namespace std;
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.fmx"
+TForm1 *Form1;
+//---------------------------------------------------------------------------
+__fastcall TForm1::TForm1(TComponent* Owner)
+	: TForm(Owner)
+{
+}
+
+vector<string> parseCommaDelimitedString(string line){
+vector<string> result;
+stringstream s_stream(line);
+while(s_stream.good()){
+string substr;
+getline(s_stream,substr,',');
+result.push_back(substr);
+}
+return result;
+}
+
+const char* converToCharPtr(AnsiString ansiStr){
+return ansiStr.c_str();
+}
+
+
+
+void __fastcall TForm1::RegistrationButtonClick(TObject *Sender)
+{
+fstream accountDB;
+  accountDB.open("registeredUsers.txt",ios::app);
+if(accountDB.is_open()){
+AnsiString username = RegistrationUsernameEdit->Text;
+AnsiString password = RegistrationPasswordEdit->Text;
+accountDB<<username<<","<<password<<"\n";
+accountDB.close();
+RegistrationPanel->Visible=false;
+LoginPanel->Visible=true;
+}
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::LoginPageButtonClick(TObject *Sender)
+{
+ RegistrationPanel->Visible=false;
+ LoginPanel->Visible=true;
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::LoginButtonClick(TObject *Sender)
+{
+ fstream accountDB;
+accountDB.open("registeredUsers.txt",ios::in);
+if(accountDB.is_open()){
+string line;
+while( getline(accountDB,line)){
+vector<string> parsedLine=parseCommaDelimitedString(line);
+
+const char* username = parsedLine.at(0).c_str();
+AnsiString editUsername = LoginUsernameEdit->Text;
+const char* usernameString = converToCharPtr(editUsername);
+bool validUsername = false;
+
+const char* password = parsedLine.at(1).c_str();
+AnsiString editPassword = LoginPasswordEdit->Text;
+const char* passwordString = converToCharPtr(editPassword);
+bool validPassword = false;
+
+if(strcmp(username,usernameString)==0){validUsername = true;}
+if(strcmp(password,passwordString)==0){validPassword = true;}
+if(validUsername && validPassword){
+   QuestForm->Show();
+
+}
+
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::RegistrationPageButtonClick(TObject *Sender)
+{
+RegistrationPanel->Visible=true;
+ LoginPanel->Visible=false;
+}
+//---------------------------------------------------------------------------
+
---
+++ Quest.h
@@ -0,0 +1,20 @@
+//---------------------------------------------------------------------------
+
+#ifndef QuestH
+#define QuestH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <FMX.Controls.hpp>
+#include <FMX.Forms.hpp>
+//---------------------------------------------------------------------------
+class TQuestForm : public TForm
+{
+__published:	// IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+	__fastcall TQuestForm(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TQuestForm *QuestForm;
+//---------------------------------------------------------------------------
+#endif
